a45da698b9c125e69b40efe299d862670e632612
zerodegress <zerodegress@outlook.com>
2026-03-07 11:21:49 +0800

    docs:Refactor global API and introduce PlantBase
    Remove the `_G.` prefix from global API objects (`game`,
    `PlantClassManager`)
    for cleaner code. Introduce `PlantBase` to provide a structured,
    object-oriented foundation for defining custom plant classes.

--- library/fvm_lite_lua_api.lua
+++ library/fvm_lite_lua_api.lua
@@ -4,78 +4,77 @@
 
 --- @class GameAPI
 --- The global 'game' object provides an interface to the underlying game engine.
-_G.game = {}
+game = {}
 
 --- Logs a message to the game's console or log file.
 --- @param message string The message to log.
-function _G.game.logMessage(message) end
+function game.logMessage(message) end
 
 --- Gets the platform the game is currently running on.
 --- @return string "windows"|"android"|"linux" The name of the platform.
-function _G.game.getPlatform() end
+function game.getPlatform() end
 
 --- Gets the external storage path (primarily relevant for Android).
 --- @return string The external storage path.
-function _G.game.getExternalStoragePath() end
+function game.getExternalStoragePath() end
 
 --- Checks if a file exists at the given path.
 --- @param path string The path to the file.
 --- @return boolean `true` if the file exists, `false` otherwise.
-function _G.game.fileExists(path) end
+function game.fileExists(path) end
 
 --- Checks if a directory exists at the given path.
 --- @param path string The path to the directory.
 --- @return boolean `true` if the directory exists, `false` otherwise.
-function _G.game.dirExists(path) end
+function game.dirExists(path) end
 
 --- Registers a plant class with the game engine. Usually called internally by PlantClassManager.
 --- @param className string The unique name of the plant class.
 --- @param classTable table The Lua table defining the plant class logic.
-function _G.game.registerPlantClass(className, classTable) end
+function game.registerPlantClass(className, classTable) end
 
 --- Creates a new instance of a Lua-defined plant.
 --- @param luaClassName string The name of the Lua plant class to create (must be registered).
 --- @param ident string A unique identifier for this plant instance.
 --- @return table|nil The created plant instance table, or `nil` if creation fails.
-function _G.game.createLuaPlant(luaClassName, ident) end
+function game.createLuaPlant(luaClassName, ident) end
 
 --- Creates a new instance of a standard (non-Lua defined) plant.
 --- @param ident string A unique identifier for this plant instance.
 --- @return table|nil The created plant instance table, or `nil` if creation fails.
-function _G.game.createPlant(ident) end
+function game.createPlant(ident) end
 
 --- Registers a map with the game.
 --- @param ident string A unique identifier for the map.
 --- @param mapDataJsonString string A JSON string representing the map data.
 --- @return boolean `true` if registered successfully, `false` otherwise.
-function _G.game.registerMap(ident, mapDataJsonString) end
+function game.registerMap(ident, mapDataJsonString) end
 
 --- Gets a property from a plant instance.
 --- @param selfPtr table The plant instance table.
 --- @param propName string The name of the property to get.
 --- @return any The value of the property.
-function _G.game.getPlantProperty(selfPtr, propName) end
+function game.getPlantProperty(selfPtr, propName) end
 
 --- Sets a property on a plant instance.
 --- @param selfPtr table The plant instance table.
 --- @param propName string The name of the property to set.
 --- @param value any The new value for the property.
 --- @return boolean `true` if the property was set successfully, `false` otherwise.
-function _G.game.setPlantProperty(selfPtr, propName, value) end
+function game.setPlantProperty(selfPtr, propName, value) end
 
 --- Output Message in game window.
 --- @param msg string Message to output.
 --- @param colorCode integer Color hex code.
 --- @return boolean `true` if the property was set successfully, `false` otherwise.
-function _G.game.outMessage(msg, colorCode) end
+function game.outMessage(msg, colorCode) end
 
---- @class PlantClassManagerAPI
 --- Manages the registration and lifecycle of Lua-defined plant classes.
-_G.PlantClassManager = {}
+PlantClassManager = {}
 
 --- @type table<string, table> A table containing all registered plant classes, indexed by their class name.
---- This table directly references `_G._PLANT_CLASSES`.
-_G.PlantClassManager.classes = {}
+--- This table directly references `_PLANT_CLASSES`.
+PlantClassManager.classes = {}
 
 --- Registers a new plant class. This is the primary method for mods to add new plant types.
 --- It internally sends a 'register_plant_class' message via the event system.
@@ -83,41 +82,124 @@ _G.PlantClassManager.classes = {}
 --- @param classTable table The Lua table that defines the plant class (e.g., containing `new`, `update` methods).
 --- @param modName string The name of the mod that is registering this plant class.
 --- @return boolean `true` if the class was registered successfully, `false` otherwise. (Actual return depends on event listener)
-function _G.PlantClassManager:registerClass(className, classTable, modName) end
+function PlantClassManager:registerClass(className, classTable, modName) end
 
 --- Retrieves a registered plant class by its name.
 --- This method directly accesses the `classes` table.
 --- @param className string The name of the plant class to retrieve.
 --- @return table|nil The plant class table if found, or `nil` otherwise.
-function _G.PlantClassManager:getClass(className) end
+function PlantClassManager:getClass(className) end
 
 --- Returns a list of all registered plant class names.
 --- @return string[] A list of class names.
-function _G.PlantClassManager:listClasses() end
+function PlantClassManager:listClasses() end
 
 --- Creates a new instance of a plant.
 --- This method internally triggers a 'create_plant' event via the event system.
 --- @param className string The name of the plant class to instantiate (must be registered).
 --- @param ident string A unique identifier for this plant instance.
 --- @return table|nil The created plant instance table, or `nil` if creation fails.
-function _G.PlantClassManager:createPlant(className, ident) end
-
-
---- @type table<string, table>
---- A global table used by the game engine (possibly Delphi/C++ backend) to store all registered plant class definitions.
---- Mods usually interact with this indirectly via `PlantClassManager`.
-_G._PLANT_CLASSES = {}
+function PlantClassManager:createPlant(className, ident) end
 
 --- @class EventSystemAPI
----The global eventSystem object manages event dispatching and inter-mod communication.
+--- The global eventSystem object manages event dispatching and inter-mod communication.
 ---
----Methods:
+--- Registers an event listener. Return false from callback to stop propagation.
 --- @field register fun(self: EventSystemAPI, eventName:string, callback:fun(arg1:any, ...:any):boolean|nil, priority:integer?, modName:string?):nil
----  Registers an event listener. Return false from callback to stop propagation.
+--- Triggers an event. Returns true if all listeners processed it, false if propagation stopped.
 --- @field trigger fun(self: EventSystemAPI, eventName:string, ...:any):boolean
----  Triggers an event. Returns true if all listeners processed it, false if propagation stopped.
+--- Send message to other mods.
 --- @field sendMessage fun(self: EventSystemAPI, sender:string, msgType:string, data:any):nil
+--- receive message from other mods.
 --- @field subscribe fun(self: EventSystemAPI, msgType:string, callback:fun(sender:string, data:any), modName:string?):nil
 --- @field unsubscribe fun(self: EventSystemAPI, msgType:string, modName:string):nil
 --- @field processMessages fun(self: EventSystemAPI):nil
 --- @field unregisterByMod fun(self: EventSystemAPI, modName:string):nil
+
+--- Plant base class, all plants should inherit from this base class.
+--- @class PlantBase
+PlantBase = {}
+
+--- Create new plant class(not instance).
+--- @generic T:PlantBase
+--- @param className string Plant class name.
+--- @return T
+function PlantBase:new(className) end
+
+--- Register a plant class(recommended to use)
+--- @param className string Plant class name.
+--- @param modName string Mod name.
+--- @return PlantBase
+function PlantBase:register(className, modName) end
+
+--- Get the delphi object of a plant instance.
+--- @param instance PlantBase Plant instance.
+--- @return unknown
+function PlantBase:getSelf(instance) end
+
+--- Get property of a plant instance(via Delphi).
+--- @param instance PlantBase Plant instance.
+--- @param propName string Property name.
+--- @return any Property value.
+function PlantBase:getProperty(instance, propName) end
+
+--- Set property of a plant instance(via Delphi).
+--- @param instance PlantBase Plant instance.
+--- @param propName string Property name.
+--- @param value any Property value.
+--- @return boolean if the operation succeed.
+function PlantBase:setProperty(instance, propName, value) end
+
+--- Call method of the plant instance(via Delphi).
+--- @param instance PlantBase Plant instance.
+--- @param methodName string Method name.
+--- @param ... any[] Call parameters.
+--- @return any The method return.
+function PlantBase:callMethod(instance, methodName, ...) end
+
+PlantBase.__index = {}
+
+--- Initialize the plant instance.
+--- This method is typically called when a new plant instance is created.
+--- Should be overrided.
+function PlantBase.__index:init() end
+
+--- Run the plant instance's update logic.
+--- Should be overrided.
+--- @param delta number Time elapsed since the last frame.
+function PlantBase.__index:run(delta) end
+
+--- Render the plant instance.
+--- Should be overrided.
+--- @param progress number Current progress of the plant.
+--- @param opacity number Opacity of the plant.
+function PlantBase.__index:render(progress, opacity) end
+
+--- Log a message associated with the plant instance.
+--- @param message string The message to log.
+function PlantBase.__index:log(message) end
+
+--- Get the internal Delphi object pointer for this plant instance.
+--- This is a convenience method that calls `PlantBase:getSelf(self)`.
+--- @return unknown The internal Delphi object.
+function PlantBase.__index:getSelfPtr() end
+
+--- Get a property of this plant instance.
+--- This is a convenience method that calls `PlantBase:getProperty(self, propName)`.
+--- @param propName string Property name.
+--- @return any Property value.
+function PlantBase.__index:get(propName) end
+
+--- Set a property of this plant instance.
+--- This is a convenience method that calls `PlantBase:setProperty(self, propName, value)`.
+--- @param propName string Property name.
+--- @param value any Property value.
+--- @return boolean if the operation succeed.
+function PlantBase.__index:set(propName, value) end
+
+--- Call a method on this plant instance.
+--- This is a convenience method that calls `PlantBase:callMethod(self, methodName, ...)`.
+--- @param methodName string Method name.
+--- @param ... any[] Call parameters.
+--- @return any The method return.
+function PlantBase.__index:call(methodName, ...) end
